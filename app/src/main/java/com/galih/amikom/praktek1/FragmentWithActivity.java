1a797504b7a7342e926d0ce2181eb221828f6888
galihmay <galihmaya1@gmail.com>
2018-10-08 20:20:10 +0700

    Tugas Pertemuan 3

---
+++ app/src/main/java/com/galih/amikom/praktek1/FragmentWithActivity.java
@@ -0,0 +1,36 @@
+package com.galih.amikom.praktek1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class FragmentWithActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Fragment");
+        setContentView(R.layout.activity_with_fragment);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+       /* *getSupportFragmentManager = inisialisasi nilai FragmentManager untuk berinteraksi
+            dengan Activity saat ini
+         */
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+
+        FirstFragment FirstFragment = new FirstFragment();
+
+        transaction.add(R.id.frame_content, FirstFragment);
+
+
+        transaction.commit();
+
+
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/galih/amikom/praktek1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.galih.amikom.praktek1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import java.util.HashMap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.galih.amikom.praktek1.util.PreferencesHelper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    PreferencesHelper session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        session = new PreferencesHelper(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.lblName);
+
+
+
+
+
+        if(session.checkLogin())
+            finish();
+
+
+        HashMap<String, String> user = session.getUserDetails();
+
+
+        String name = user.get(PreferencesHelper.KEY_NAME);
+
+
+
+
+        lblName.setText(Html.fromHtml("Hello: <b>" + name + "</b>"));
+
+
+    }
+
+
+    public void intent (View view) {
+        Intent intent =new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+
+    }
+    public void fragment (View view) {
+        Intent intent =new Intent(MainActivity.this,FragmentWithActivity.class);
+        startActivity(intent);
+
+    }
+
+}
